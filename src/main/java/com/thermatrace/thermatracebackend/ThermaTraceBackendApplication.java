873d53b02006b04a7358220a2e61427d39168a58
JeanGrandez <jpmle10@hotmail.com>
2025-11-10 01:36:52 -0500

    chore: initial commit

---
+++ src/main/java/com/thermatrace/thermatracebackend/ThermaTraceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thermatrace.thermatracebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermaTraceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThermaTraceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thermatrace/thermatracebackend/ThermaTraceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thermatrace.thermatracebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermaTraceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
